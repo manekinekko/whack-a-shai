91a8f36a24a7af3147967e60479bba0b9946e9c7
Wassim CHEGHAM <github@wassimchegham.com>
2016-08-19 19:03:04 +0200

    ❤️✨❤️

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  list = [];
+
+  constructor() {
+    this.list = Array(12).fill(0).map( () => {
+      return { v: false };
+    });
+  }
+
+  ngOnInit() {
+    setInterval( this.show.bind(this), 1000 );
+  }
+
+  show() {
+    let item = this.list[ (Math.random() * this.list.length) | 0 ];
+    item.v = true;
+
+    setTimeout( () => {
+      item.v = false;
+    }, 300);
+
+  }
 }
